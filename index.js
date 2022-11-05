6e8809bba04f29a99a561f3636b96db6f4e08095
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-11-04 18:21:18 -0600

    active states listos

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 var button = document.querySelector('.formButton');
 var input = document.querySelector('.emailInput');
+var message = document.querySelector('.warning-message');
+var body = document.querySelector('body');
+var error = document.querySelector('.icon-error');
+
+body.addEventListener('click', function(){
+    input.style.border = "1px solid hsl(0, 36%, 70%)";
+    message.style.opacity = 0;
+    error.style.opacity = 0;
+})
 
 button.addEventListener('mouseover', function(){
     button.style.backgroundImage = "linear-gradient(135deg, hsl(0, 0%, 100%), hsl(0, 74%, 74%))";
@@ -11,14 +20,16 @@ button.addEventListener('mouseout', function(){
 
 button.addEventListener('click', function(event){
     event.preventDefault();
+    event.stopPropagation();
     input.style.border = "3px solid hsl(0, 93%, 68%)";
-    input.stlye.backgroundImage = 'url("images/icon-error.svg")';
+    message.style.opacity = 1;
+    error.style.opacity = 1;
 })
 
-input.addEventListener('focus', function(){
+input.addEventListener('click', function(event){
+    event.stopPropagation();
     input.style.border = "2px solid hsl(0, 36%, 70%)";
+    message.style.opacity = 0;
+    error.style.opacity = 0;
 })
 
-input.addEventListener('blur', function(){
-    input.style.border = "1px solid hsl(0, 36%, 70%)";
-})
