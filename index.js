574eaa4186d1e74d707122d556553aea3917fcf9
Ricardo Fuentes Urbina <ricardo.furbina@alumnos.udg.mx>
2022-11-09 17:11:47 -0600

    arreglos

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var input = document.querySelector('.emailInput');
 var message = document.querySelector('.warning-message');
 var body = document.querySelector('body');
 var error = document.querySelector('.icon-error');
+var form = document.querySelector('form');
 
 body.addEventListener('click', function(){
     input.style.border = "1px solid hsl(0, 36%, 70%)";
@@ -18,12 +19,18 @@ button.addEventListener('mouseout', function(){
     button.style.backgroundImage = "linear-gradient(135deg, hsl(0, 80%, 86%), hsl(0, 74%, 74%))";
 })
 
-button.addEventListener('click', function(event){
+form.addEventListener('submit', function(event){
     event.preventDefault();
     event.stopPropagation();
-    input.style.border = "3px solid hsl(0, 93%, 68%)";
-    message.style.opacity = 1;
-    error.style.opacity = 1;
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if(input.value == '' || regex.test(input.value) == false){
+        input.style.border = "3px solid hsl(0, 93%, 68%)";
+        message.style.opacity = 1;
+        error.style.opacity = 1; 
+    }else{
+        console.log('correct');
+    }
+    
 })
 
 input.addEventListener('click', function(event){
